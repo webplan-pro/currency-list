fa80f3fb334ea1a7fbba1a1d68e0bb7112f50388
Arturas Sotnicenko <arturas@scale3c.com>
2020-12-05 17:04:32 +0200

    added currency list data with locales and class to retrieve them

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export const Greeter = (name: string) => `Hello ${name}`; 
+// @ts-ignore
+import * as currencyList from "./currency-list.json"
+class CurrencyList {
+    currencyList: {
+        [locale: string]: {
+            [code: string]: {
+                name: string,
+                symbol_native: string,
+                symbol: string,
+                code: string,
+                name_plural: string,
+                rounding: number,
+                decimal_digits: number
+            }
+        }
+    }
+
+    constructor() {
+        this.currencyList = currencyList
+    }
+    public getAll() {
+        return this.currencyList
+    }
+    public get(currency_code: string, locale_code: string = 'en_US') {
+        return this.currencyList[locale_code][currency_code]
+    }
+}
+export default new CurrencyList()
